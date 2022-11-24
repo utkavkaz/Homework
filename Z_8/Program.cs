124eb1760b0e0c80dedbc5ef3faf2893743a5530
utkavaz <ntkavkaz@rambler.ru>
2022-11-24 23:17:48 +0300

    homework

---
+++ Z_8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 2)
+{
+    Console.WriteLine("Вы ввели некорректное число");
+}
+else
+{
+    int num = 2;
+
+    while (num <= a)
+    {
+        Console.Write($"{num} ");
+        num = num + 2;
+    }
+}
